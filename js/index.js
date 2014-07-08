a9a3390b50557907eecd7d075e9afe06393ac288
Jordan MacDonald <jordan@wastedintelligence.com>
2014-07-07 22:21:55 -0400

    Use CSS animation when showing download menu.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.download .button').click(function() {
-    if( $('.download ul').is(':visible') ) {
-      $('.download ul').hide();
+    if( $('.download ul').css('opacity') == '0' ) {
+      $('.download ul').transition({ opacity: 1 }, 150);
       $('.download .button').removeClass('connected');
     }
     else {
-      $('.download ul').show();
+      $('.download ul').transition({ opacity: 0 }, 150);
       $('.download .button').addClass('connected');
     }
   });
